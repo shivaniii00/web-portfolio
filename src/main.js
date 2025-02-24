5bf80ef5df58452b40b981c10a95e1201ffb91a0
shivaniii00 <shivanichaturvedi606@gmail.com>
2025-02-24 18:35:39 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import * as THREE from "https://unpkg.com/three@0.128.0/build/three.module.js";
 import { GLTFLoader } from "https://unpkg.com/three@0.128.0/examples/jsm/loaders/GLTFLoader.js";
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/controls/OrbitControls.js';
-import { EffectComposer } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/postprocessing/UnrealBloomPass.js';
-import { Reflector } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/objects/Reflector.js';
-import { ShaderPass } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/postprocessing/ShaderPass.js';
+import { OrbitControls } from "https://unpkg.com/three@0.128.0/examples/jsm/controls/OrbitControls.js";
+import { EffectComposer } from "https://unpkg.com/three@0.128.0/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "https://unpkg.com/three@0.128.0/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "https://unpkg.com/three@0.128.0/examples/jsm/postprocessing/UnrealBloomPass.js";
+import { Reflector } from "https://unpkg.com/three@0.128.0/examples/jsm/objects/Reflector.js";
+import { ShaderPass } from "https://unpkg.com/three@0.128.0/examples/jsm/postprocessing/ShaderPass.js";
+
 
 
 const scene = new THREE.Scene();
